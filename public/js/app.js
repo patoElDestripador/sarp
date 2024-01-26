d8a21cc031da91a27cf237eb21836a583d759f8d
pepenauta17 <patoasesino69@gamil.com>
2024-01-26 08:11:52 -0500

    cambios de style

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+//En este app se cargaran funciones necesarias en todas las paginas 
+
+  function mensajeFuncionDesactivada() {
+    alert(
+      "Esta funcion se encuntra desactivada temporalmente por favor intentar despues"
+    );
+  }
+
+  function cambioIdioma() {
+    //apartir de aqui se generaria el cambio de leguaje de la pagina
+  }
+
+
+
+
