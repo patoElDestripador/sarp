9aa1e2ca643dfbf7e8ea906a49ea2f6b4f7e27c9
Johan Arboleda <jhse757@gmail.com>
2024-02-02 20:29:54 -0500

    se montan img

--- public/js/index.js
+++ public/js/index.js
@@ -49,4 +49,10 @@ document.getElementById("idCorazon")?.addEventListener("click",()=>{
     //funcion secreta
 });
 
+// Menu toggle
+const menu=document.querySelector(".menu");
+const toggle=document.querySelector(".toggle");
+toggle.addEventListener("click",()=>{
+  menu.classList.toggle("active");
+})
 
